7def4a86dd345324349a8e54060942bec83422d7
kanerin <kanerin1004@gmail.com>
2024-10-28 23:34:09 +0900

    change sslmode

--- backend/main.go
+++ backend/main.go
@@ -24,7 +24,7 @@ func main() {
             " dbname=" + os.Getenv("DATABASE") + 
             " host=" + os.Getenv("DB_HOST") + 
             " port=" + os.Getenv("PORT") + 
-            " sslmode=disable"
+            " sslmode=require"
 
     // リトライ処理の設定
     maxRetries := 6        // 最大リトライ回数
